921d764ed4cce5b293bccfe35848da5e92e9a3bc
Alex Aaron <alexfrancisaaron@gmail.com>
2026-02-16 12:49:59 -0600

    added new data to data.js

--- data/data.js
+++ data/data.js
@@ -1,22 +1,102 @@
-const films = [
+const collection = [
   {
-    title: 'Rear Window',
-    director: 'Alred Hitchcock',
-    year: 1954,
-    runTime: {
-      length: 110,
-      units: 'Minutes'
-    },
-    cast: [
-      {
-        name: 'Jimmy Stewart'
-      },
-      {
-        name: 'Grace Kelly'
-      },
-      {
-        name: 'Thelma Ritter'
-      }
-    ]
+      title: 'Rear Window',
+      year: 1954,
+      director: 'Alfred Hitchcock',
+      digitalCode: null,
+      genreTags: ['Mystery', 'Suspense'],
+      specialFeatures: [
+          {
+              type: 'Documentary',
+              title: "Rear Window Ethics: An Original Documentary",
+              length: '30 minutes'
+          },
+          {
+              type: 'Commentary',
+              title: 'Feature Commentary with John Fawell',
+              length: '110 minutes'
+          },
+          {
+              type: 'Interview',
+              title: 'Hitchcock/Truffaut Interview Excerpts',
+              length: '20 minutes'
+          }
+      ]
   },
-]
+  {
+      title: 'Psycho',
+      year: 1960,
+      director: 'Alfred Hitchcock',
+      digitalCode: null,
+      genreTags: ['Horror', 'Suspense'],
+      specialFeatures: [
+          {
+              type: 'Documentary',
+              title: 'The Making of Psycho',
+              length: '45 minutes'
+          },
+          {
+              type: 'Commentary',
+              title: 'Feature Commentary with Stephen Rebello',
+              length: '109 minutes'
+          }
+      ]
+  },
+  {
+      title: 'The Thing',
+      year: 1982,
+      director: 'John Carpenter',
+      digitalCode: null,
+      genreTags: ['Horror', 'Science Fiction'],
+      specialFeatures: [
+          {
+              type: 'Commentary',
+              title: 'Feature Commentary with Star Kurt Russell and Director John Carpenter',
+              length: '109 minutes'
+          },
+      ]
+  },
+  {
+      title: 'Big Trouble in Little China',
+      year: 1986,
+      director: 'John Carpenter',
+      digitalCode: null,
+      genreTags: ['Comedy', 'Fantasy'],
+      specialFeatures: [
+          {
+              type: 'Commentary',
+              title: 'Audio Commentary with Director John Carpenter and Actor Kurt Russell',
+              length: '110 minutes'
+          },
+          {
+              type: 'Alternate Edits',
+              title: 'Deleted Scenes',
+              length: '10 inutes'
+          },
+          {
+              type: 'Interview',
+              title: 'Vintage Interviews with Cast and Crew',
+              length: '15 minutes'
+          }
+      ]
+  },
+  {
+      title: 'Fright Night',
+      year: 1985,
+      director: 'Tom Holland',
+      digitalCode: null,
+      genreTags: ['Horror', 'Vampire'],
+      specialFeatures: [
+          {
+              type: 'Documentary',
+              title: "'You're So Cool, Brewster!' Documentary",
+              length: '35 minutes'
+          },
+          {
+              type: 'Interview',
+              title: 'First Ever Fright Night Reunion Panel',
+              length: '60 minutes'
+          }
+      ]
+  }
+];
