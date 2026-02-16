c078b40fc238723483a3e761f2432bd6ed9521ef
Alex Aaron <alexfrancisaaron@gmail.com>
2026-02-16 12:40:27 -0600

    added file structure and data.js

---
+++ data/data.js
@@ -0,0 +1,22 @@
+const films = [
+  {
+    title: 'Rear Window',
+    director: 'Alred Hitchcock',
+    year: 1954,
+    runTime: {
+      length: 110,
+      units: 'Minutes'
+    },
+    cast: [
+      {
+        name: 'Jimmy Stewart'
+      },
+      {
+        name: 'Grace Kelly'
+      },
+      {
+        name: 'Thelma Ritter'
+      }
+    ]
+  },
+]
