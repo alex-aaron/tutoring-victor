053607f4417b1feb01ec80dac3747cb63ffe80d0
Alex Aaron <alexfrancisaaron@gmail.com>
2026-02-23 08:29:14 -0600

    added index.html, filter prompts

---
+++ filter.js
@@ -0,0 +1,39 @@
+/*
+Implement the function getTwoWordTitles that takes in an array of movie objects.
+This function should use the native filter method return a new array of only the
+movie objects whose title is just two words.
+*/
+
+function getTwoWordTitles(){
+
+}
+
+/*
+Implement the function getShortSpecialFeatures that takes in an array of movie objects.
+This function should use the native filter method to return a new array of only the movie
+objects that have a special feature that is less than 30 minutes.
+*/
+
+function getShortSpecialFeatures(){
+
+}
+
+/*
+Implement the function getCommentaries that takes in an array of movie objects. This function
+should use the native filter method to return a new array of only the movies that have a special
+feature whose type is "Commentary"
+*/
+
+function getCommentaries(){
+
+}
+
+/*
+Implement the function getEightiesHorror that takes in an array of movie objects. This function
+should use the native filter method to return a new array of only the films that were made in
+the 1980s and have 'Horror' in their genreTags array.
+*/
+
+function getEightiesHorror(){
+  
+}
